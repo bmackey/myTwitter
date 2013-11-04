e5e0a2d62cdcb1917dd91c22e35a880d2b6e79c1
Brid Mackey <brid.mackey@gmail.com>
2013-11-04 00:02:06 +0000

    finishing user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Mytwitter::Application.routes.draw do
   resources :users
 
-  get "static_pages/home"
+  # get "static_pages/home"
 
-  get "static_pages/help"
+  # get "static_pages/help"
 
-  get "static_pages/about"
+  # get "static_pages/about"
 
-  get "static_pages/contact"
+  # get "static_pages/contact"
 
-  get "static_pages/test1"
+  # get "static_pages/test1"
 
 
   root to: 'static_pages#home'
+  match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
