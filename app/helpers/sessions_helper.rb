4d5fb1a717c7c605fa686b40dfe92a5311ef0e2e
Brid Mackey <brid.mackey@gmail.com>
2013-12-03 23:55:00 +0000

    add user micropost

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,9 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def current_user=(user)
-      @current_user = user
-    end
+  def current_user?(user)
+    user == current_user
+  end
 
     def current_user
       if @current_user.nil?
